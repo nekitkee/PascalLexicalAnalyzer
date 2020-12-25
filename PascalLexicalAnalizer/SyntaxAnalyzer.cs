a859e14486f553e8dd8b5b491833284ed8fb9bcf
Nick Whynter <nekitkee@gmail.com>
2020-12-25 15:38:57 +0200

    Finished

--- PascalLexicalAnalizer/SyntaxAnalyzer.cs
+++ PascalLexicalAnalizer/SyntaxAnalyzer.cs
@@ -19,28 +19,37 @@ namespace PascalLexicalAnalizer
             {
                 {
                     "IfStatement",
-                    new List<Action>{
-                () => Word("if") ,  LogicalRelation, ()=> Word("then"), FunctionCall, () => Word(";") , LexemListisEmpty }
+                    new List<Action>{ () => Word("if") ,  LogicalRelation, ()=> Word("then"), FunctionCall, () => Word(";") , LexemListisEmpty }
                 },
-
                 {
                     "LogicalRelation",
-                    new List<Action> {
-                Property, RelationOperator , LiteralOrIdentifier}
+                    new List<Action> { Property, RelationOperator , LiteralOrIdentifier}
                 },
-
                 {
                     "FunctionCall",
-                    new List<Action> {
-                Identifier, () => Word("("), LiteralOrIdentifier, () => Word(")") }
+                    new List<Action> { Identifier, () => Word("("), LiteralOrIdentifier, () => Word(")") }
                 },
-
                 {
                     "Property",
-                    new List<Action> {
-                Identifier, () => Word("."), Identifier }
+                    new List<Action> { Identifier, () => Word("."), Identifier }
                 }
             };
+        }
+
+        private void ParseSyntaxSequence(List<Action> parsersList) => parsersList.ForEach(p => p.Invoke());
+        private void IfStatement() => ParseSyntaxSequence(_syntax["IfStatement"]);
+        private void LogicalRelation() => ParseSyntaxSequence(_syntax["LogicalRelation"]);
+        private void Property() => ParseSyntaxSequence(_syntax["Property"]);
+        private void FunctionCall()
+        {
+            try
+            {
+                ParseSyntaxSequence(_syntax["FunctionCall"]);
+            }
+            catch
+            {
+                throw new Exception($"Function call expected");
+            }
         }
 
         public void Analyze() 
@@ -53,23 +62,6 @@ namespace PascalLexicalAnalizer
             catch(Exception ex)
             {
                 Result = "error! " +  ex.Message;
-            }
-        }
-
-        private void ParseSyntaxSequence(List<Action> parsersList) => parsersList.ForEach(p => p.Invoke());
-        private void IfStatement() => ParseSyntaxSequence(_syntax["IfStatement"]);
-        private void LogicalRelation() => ParseSyntaxSequence(_syntax["LogicalRelation"]);
-        private void Property() => ParseSyntaxSequence(_syntax["Property"]);
-
-        private void FunctionCall()
-        {
-            try
-            {
-                ParseSyntaxSequence(_syntax["FunctionCall"]);
-            }
-            catch
-            {
-                throw new Exception($"Function call expected");
             }
         }
 
@@ -92,19 +84,15 @@ namespace PascalLexicalAnalizer
         private void Word(string word) =>
             ParseNextLexem(predicate:() => (_currentLex.value == word), 
                         errorMessage:() => $"{word} expected");
-
         private void Identifier() =>
              ParseNextLexem(predicate:() => (_currentLex.type == LexType.IDN), 
                          errorMessage:() => "Identificator expected");
-
         private void RelationOperator() =>
             ParseNextLexem(predicate:() => (_relationOperatorsList.Contains(_currentLex.value)), 
                         errorMessage:() => $"unknown relation operator {_currentLex.value}");
-
         private void LiteralOrIdentifier() =>
             ParseNextLexem(predicate:() => (_currentLex.type == LexType.IDN || _currentLex.type == LexType.LIT), 
                         errorMessage:() => "Literal or identifier expected");
-
         private bool GetNextLex()
         {
             if (_currentLexIndex < _lexemes.Count)
